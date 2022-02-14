3728be06a7a6eb39481d50611e9eb6c867777b53
Jorge Prendes <jorge.prendes@gmail.com>
2022-02-14 16:42:09 +0000

    Download coremark at build  rather than configure

--- cmake/coremark.cmake
+++ cmake/coremark.cmake
@@ -1,7 +1,11 @@
-include(FetchContent)
+include(ExternalProject)
+include(cmake/utils.cmake)
 
-FetchContent_Declare(coremark-src
+ExternalProject_Add(coremark-src
     URL https://github.com/eembc/coremark/archive/refs/heads/main.zip
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/coremark-src
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
 )
-FetchContent_MakeAvailable(coremark-src)
-FetchContent_GetProperties(coremark-src SOURCE_DIR COREMARK_DIR)
+get_project_prop(coremark-src SOURCE_DIR COREMARK_DIR)
